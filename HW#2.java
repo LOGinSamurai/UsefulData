db43a3cbd2996b799e63e5c6285a8a566e3c991e
LOGinSamurai <52347454+LOGinSamurai@users.noreply.github.com>
2019-10-11 19:31:20 +0300

    Create HW#2.java

---
+++ HW#2.java
@@ -0,0 +1,74 @@
+  public class Box {
+	 public static Integer value = 0;
+    public static void main(String[] args) {
+        Incrementer Obj1=new Incrementer();
+        Decrementer Obj2=new Decrementer();
+        Printer Obj3=new Printer();
+          Obj1.start();
+          Obj2.start();
+          Obj3.start();
+    }
+
+
+}
+
+ 
+class Incrementer extends Thread {
+
+    public void increment() {
+        if (Box.value<=3) {
+            Box.value++;}
+
+    }
+
+    public void run() {
+        while (true) {
+            increment();
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
+
+
+class Decrementer extends Thread {
+    public void decrement() {
+        if (Box.value >= -3) {
+            Box.value--;
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void run() {
+        while (true) {
+            decrement();
+        }
+    }
+}
+
+class Printer extends Thread {
+    public void print() {
+        if (Box.value>=0) {System.out.println(Box.value);
+                    }else System.err.print(Box.value);
+    }
+
+    public void run() {
+        while (true) {
+            print();
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
